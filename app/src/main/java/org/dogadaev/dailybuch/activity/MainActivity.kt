522e0c14c690d39372dd1f599b83a9b9e7053c08
Vlad Dogadaev <vmdogad@gmail.com>
2021-05-01 16:24:32 +0200

    created MainActivity

---
+++ app/src/main/java/org/dogadaev/dailybuch/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.dogadaev.dailybuch.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.dogadaev.dailybuch.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
