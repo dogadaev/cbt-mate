7271d621fb74a54a0f270f8b7dee2116cd6561bb
Vlad Dogadaev <vmdogad@gmail.com>
2022-02-02 00:51:12 +0100

    chore: code cleanup

--- common/ui/src/main/java/org/dogadaev/ui/theme/Typography.kt
+++ common/ui/src/main/java/org/dogadaev/ui/theme/Typography.kt
@@ -22,28 +22,28 @@ private val CormorantGaramondFontFamily = FontFamily(
     )
 )
 
-//private val RalewayFontFamily = FontFamily(
-//    listOf(
-//        Font(R.font.raleway_regular),
-//        Font(R.font.raleway_italic, FontWeight.Normal, FontStyle.Italic),
-//        Font(R.font.raleway_thin, FontWeight.Thin, FontStyle.Normal),
-//        Font(R.font.raleway_thin_italic, FontWeight.Thin, FontStyle.Italic),
-//        Font(R.font.raleway_light, FontWeight.Light, FontStyle.Normal),
-//        Font(R.font.raleway_light_italic, FontWeight.Light, FontStyle.Italic),
-//        Font(R.font.raleway_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
-//        Font(R.font.raleway_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
-//        Font(R.font.raleway_bold, FontWeight.Bold, FontStyle.Normal),
-//        Font(R.font.raleway_bold_italic, FontWeight.Bold, FontStyle.Italic),
-//        Font(R.font.raleway_medium, FontWeight.Medium, FontStyle.Normal),
-//        Font(R.font.raleway_medium_italic, FontWeight.Medium, FontStyle.Italic),
-//        Font(R.font.raleway_bold, FontWeight.Bold, FontStyle.Normal),
-//        Font(R.font.raleway_bold_italic, FontWeight.Bold, FontStyle.Italic),
-//        Font(R.font.raleway_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
-//        Font(R.font.raleway_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
-//        Font(R.font.raleway_black, FontWeight.Black, FontStyle.Normal),
-//        Font(R.font.raleway_black_italic, FontWeight.Black, FontStyle.Italic),
-//    )
-//)
+private val RalewayFontFamily = FontFamily(
+    listOf(
+        Font(R.font.raleway_regular),
+        Font(R.font.raleway_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.raleway_thin, FontWeight.Thin, FontStyle.Normal),
+        Font(R.font.raleway_thin_italic, FontWeight.Thin, FontStyle.Italic),
+        Font(R.font.raleway_light, FontWeight.Light, FontStyle.Normal),
+        Font(R.font.raleway_light_italic, FontWeight.Light, FontStyle.Italic),
+        Font(R.font.raleway_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+        Font(R.font.raleway_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+        Font(R.font.raleway_bold, FontWeight.Bold, FontStyle.Normal),
+        Font(R.font.raleway_bold_italic, FontWeight.Bold, FontStyle.Italic),
+        Font(R.font.raleway_medium, FontWeight.Medium, FontStyle.Normal),
+        Font(R.font.raleway_medium_italic, FontWeight.Medium, FontStyle.Italic),
+        Font(R.font.raleway_bold, FontWeight.Bold, FontStyle.Normal),
+        Font(R.font.raleway_bold_italic, FontWeight.Bold, FontStyle.Italic),
+        Font(R.font.raleway_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+        Font(R.font.raleway_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+        Font(R.font.raleway_black, FontWeight.Black, FontStyle.Normal),
+        Font(R.font.raleway_black_italic, FontWeight.Black, FontStyle.Italic),
+    )
+)
 
 val Typography = Typography(
     defaultFontFamily = CormorantGaramondFontFamily,
